6e289722698c5c6f111410ad46709e118b4a81e5
johan0A <arnaudchesse@outlook.com>
2024-12-05 01:51:17 +0100

    HAAA

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@
 #include <cmath>
 #include <vector>
 #include <cassert>
+#include <random>
 #include <algorithm>
-
+#include <optional>
 #include <omp.h>
 
 using namespace std;
@@ -28,6 +29,42 @@ struct Config {
     ParamEntier params_entier[10];
     ParamChaine params_chaine[10];
 };
+
+optional<int> trouver_param_entier(Config& config, const string& nom) {
+    for(size_t i = 0; i < config.nb_params_entier; i++) {
+        if (config.params_entier[i].nom == nom) {
+            return config.params_entier[i].valeur;
+        }
+    }
+    return nullopt;
+}
+
+optional<string> trouver_param_chaine(Config& config, const string& nom) {
+    for(size_t i = 0; i < config.nb_params_chaine; i++) {
+        if (config.params_chaine[i].nom == nom) {
+            return config.params_chaine[i].valeur;
+        }
+    }
+    return nullopt;
+}
+
+void creer_dossiers_sortie(const string& chemin_base) {
+    if (!filesystem::exists(chemin_base)) {
+        filesystem::create_directory(chemin_base);
+    }
+    if (!filesystem::exists(chemin_base + "/images")) {
+        filesystem::create_directory(chemin_base + "/images");
+    }
+    if (!filesystem::exists(chemin_base + "/gif")) {
+        filesystem::create_directory(chemin_base + "/gif");
+    }
+}
+
+string extraire_nom_fichier(const string& chemin_fichier) {
+    size_t derniere_barre = chemin_fichier.find_last_of("/\\");
+    size_t dernier_point = chemin_fichier.find_last_of(".");
+    return chemin_fichier.substr(derniere_barre + 1, dernier_point - derniere_barre - 1);
+}
 
 void affichage_param_entier(ParamEntier params_entier) {
     cout << params_entier.nom << " = " << params_entier.valeur << "\n";
@@ -87,52 +124,41 @@ Config lire_config(string fichier_nom) {
     return result;
 }
 
-
-void bruitage(Image_PNG *image_originale, Image_PNG *image_bruiter, int force) {
-    assert(image_originale->largeur == image_bruiter->largeur);
-    assert(image_originale->hauteur == image_bruiter->hauteur);
-
-
-    #pragma omp parallel for
-    for(size_t y = 0; y < image_originale->hauteur; y++) {
-        for(size_t x = 0; x < image_originale->largeur; x++) {
-            RVB pixel = image_originale->pixels[y][x];
-
-            image_bruiter->pixels[y][x].rouge = clamp(rand() % force - force/2 + pixel.rouge, 0, 255);
-            image_bruiter->pixels[y][x].vert  = clamp(rand() % force - force/2 + pixel.vert, 0, 255);
-            image_bruiter->pixels[y][x].bleu  = clamp(rand() % force - force/2 + pixel.bleu, 0, 255);
+void assombrir_image(const Image_PNG& image_source, Image_PNG& image_destination, double ratio) {
+    for (size_t y = 0; y < image_source.hauteur; ++y) {
+        for (size_t x = 0; x < image_source.largeur; ++x) {
+            RVB pixel_source = image_source.pixels[y][x];
+            image_destination.pixels[y][x] = {
+                static_cast<Composante>(pixel_source.rouge * ratio),
+                static_cast<Composante>(pixel_source.vert  * ratio),
+                static_cast<Composante>(pixel_source.bleu  * ratio)
+            };
         }
     }
 }
 
-void creer_fondu_bruitage(const string fichier_path, size_t nb_etapes, double somme_max) {
-    Image_PNG image = charger_PNG(fichier_path);
-    size_t lastSlash = fichier_path.find_last_of("/\\");
-    size_t lastDot = fichier_path.find_last_of(".");
-    string nom_fichier = fichier_path.substr(lastSlash + 1, lastDot - lastSlash - 1);
-
-    if (!filesystem::exists("out")) {
-        filesystem::create_directory("out");
+void convertir_niveaux_gris(const Image_PNG& image_source, Image_PNG& image_destination, double ratio) {
+    for (size_t y = 0; y < image_source.hauteur; ++y) {
+        for (size_t x = 0; x < image_source.largeur; ++x) {
+            RVB pixel = image_source.pixels[y][x];
+            double luminosite = pixel.rouge * 0.299 + pixel.vert * 0.587 + pixel.bleu * 0.114;
+            image_destination.pixels[y][x] = {
+                static_cast<Composante>(pixel.rouge * (1-ratio) + luminosite * ratio),
+                static_cast<Composante>(pixel.vert  * (1-ratio) + luminosite * ratio),
+                static_cast<Composante>(pixel.bleu  * (1-ratio) + luminosite * ratio)
+            };
+        }
     }
+}
 
-    if (!filesystem::exists("out/images")) {
-        filesystem::create_directory("out/images");
-    }
-
-    if (!filesystem::exists("out/gif")) {
-        filesystem::create_directory("out/gif");
-    }
-    
-    Image_PNG image_buffer = creer_PNG(image.hauteur, image.largeur);
-    
-    for(size_t i = 0; i < nb_etapes; i++) {
-        double somme = (somme_max / nb_etapes) * (i + 1);
-        bruitage(&image, &image_buffer, static_cast<int>(somme));
-        sauver_PNG("out/images/" + nom_fichier + "_" + to_string(i) + ".png", image_buffer);
-    }
-
-    if (nb_etapes >= 1) {
-        generer_GIF("out/images/" + nom_fichier + "_", "out/gif/" + nom_fichier, 0, nb_etapes - 1, 15, 0);
+void bruiter_image(const Image_PNG& image_source, Image_PNG& image_destination, int intensite) {
+    for(size_t y = 0; y < image_source.hauteur; y++) {
+        for(size_t x = 0; x < image_source.largeur; x++) {
+            RVB pixel_src = image_source.pixels[y][x];
+            image_destination.pixels[y][x].rouge = clamp(rand() % intensite - intensite/2 + pixel_src.rouge, 0, 255);
+            image_destination.pixels[y][x].vert  = clamp(rand() % intensite - intensite/2 + pixel_src.vert , 0, 255);
+            image_destination.pixels[y][x].bleu  = clamp(rand() % intensite - intensite/2 + pixel_src.bleu , 0, 255);
+        }
     }
 }
 
@@ -140,23 +166,23 @@ double gaussian_function(int x, int y, double sigma) {
     return (1.0 / (2.0 * M_PI * sigma * sigma)) * std::exp(-(x * x + y * y) / (2.0 * sigma * sigma));
 }
 
-void floutage_gaussien(Image_PNG *image_originale, Image_PNG *image_floue, double sigma) {
-    assert(image_originale->largeur == image_floue->largeur);
-    assert(image_originale->hauteur == image_floue->hauteur);
+void flouter_image(const Image_PNG& image_source, Image_PNG& image_destination, double intensite) {
+    assert(image_source.largeur == image_destination.largeur);
+    assert(image_source.hauteur == image_destination.hauteur);
 
-    int kernel_size = static_cast<int>(std::ceil(2 * sigma)) + 1;
+    int kernel_size = static_cast<int>(std::ceil(2 * intensite)) + 1;
     double factor_sum = 0;
 
     for(int rx = -kernel_size/2; rx <= kernel_size/2; rx++) {
         for(int ry = -kernel_size/2; ry <= kernel_size/2; ry++) {
-            factor_sum += gaussian_function(rx, ry, sigma);
+            factor_sum += gaussian_function(rx, ry, intensite);
         }
     }
 
     #pragma omp parallel for
-    for(size_t y = 0; y < image_originale->hauteur; y++) {
-        for(size_t x = 0; x < image_originale->largeur; x++) {
-            RVB pixel = image_originale->pixels[y][x];
+    for(size_t y = 0; y < image_source.hauteur; y++) {
+        for(size_t x = 0; x < image_source.largeur; x++) {
+            RVB pixel = image_source.pixels[y][x];
 
             double r = 0;
             double g = 0;
@@ -165,25 +191,26 @@ void floutage_gaussien(Image_PNG *image_originale, Image_PNG *image_floue, doubl
             for(int ry = -kernel_size/2; ry <= kernel_size/2; ry++) {
                 for(int rx = -kernel_size/2; rx <= kernel_size/2; rx++) {
                     RVB r_pixel;
+                    // TODO: cleanup this part
                     if (
                         (y + ry) < 0 || (x + rx) < 0 || 
-                        (y + ry) >= image_originale->hauteur || 
-                        (x + rx) >= image_originale->largeur
+                        (y + ry) >= image_source.hauteur || 
+                        (x + rx) >= image_source.largeur
                     ) {
                         if (
                             (y - ry) < 0 || (x - rx) < 0 || 
-                            (y - ry) >= image_originale->hauteur || 
-                            (x - rx) >= image_originale->largeur
+                            (y - ry) >= image_source.hauteur || 
+                            (x - rx) >= image_source.largeur
                         ) {
                             r_pixel = pixel;
                         } else {
-                            r_pixel = image_originale->pixels[y - ry][x - rx];
+                            r_pixel = image_source.pixels[y - ry][x - rx];
                         }
                     } else {
-                        r_pixel = image_originale->pixels[y + ry][x + rx];
+                        r_pixel = image_source.pixels[y + ry][x + rx];
                     }
 
-                    double factor = gaussian_function(ry, rx, sigma) / factor_sum;
+                    double factor = gaussian_function(ry, rx, intensite) / factor_sum;
 
                     r += static_cast<double>(r_pixel.rouge) * factor;
                     g += static_cast<double>(r_pixel.vert)  * factor;
@@ -191,117 +218,85 @@ void floutage_gaussien(Image_PNG *image_originale, Image_PNG *image_floue, doubl
                 }
             }
 
-            image_floue->pixels[y][x].rouge = static_cast<Composante>(r);
-            image_floue->pixels[y][x].vert  = static_cast<Composante>(g);
-            image_floue->pixels[y][x].bleu  = static_cast<Composante>(b);
+            image_destination.pixels[y][x].rouge = static_cast<Composante>(r);
+            image_destination.pixels[y][x].vert  = static_cast<Composante>(g);
+            image_destination.pixels[y][x].bleu  = static_cast<Composante>(b);
         }
     }
 }
 
-void creer_fondu_flou(const string fichier_path, size_t nb_etapes, double somme_max) {
-    Image_PNG image = charger_PNG(fichier_path);
-    size_t lastSlash = fichier_path.find_last_of("/\\");
-    size_t lastDot = fichier_path.find_last_of(".");
-    string nom_fichier = fichier_path.substr(lastSlash + 1, lastDot - lastSlash - 1);
+void creer_animation_fondu_noir(const string& chemin_image, const string& chemin_destination, size_t nb_etapes) {
+    creer_dossiers_sortie(chemin_destination);
 
-    if (!filesystem::exists("out")) {
-        filesystem::create_directory("out");
-    }
+    Image_PNG image_source = charger_PNG(chemin_image);
+    string nom_image = extraire_nom_fichier(chemin_image);
+    Image_PNG image_tampon = creer_PNG(image_source.hauteur, image_source.largeur);
 
-    if (!filesystem::exists("out/images")) {
-        filesystem::create_directory("out/images");
-    }
-
-    if (!filesystem::exists("out/gif")) {
-        filesystem::create_directory("out/gif");
-    }
-    
-    Image_PNG image_buffer = creer_PNG(image.hauteur, image.largeur);
-    
     for(size_t i = 0; i < nb_etapes; i++) {
-        double somme = (somme_max / nb_etapes) * (i + 1);
-        floutage_gaussien(&image, &image_buffer, somme);
-        sauver_PNG("out/images/" + nom_fichier + "_" + to_string(i) + ".png", image_buffer);
+        double ratio = 1.0 - (static_cast<double>(i) / (nb_etapes - 1));
+        assombrir_image(image_source, image_tampon, ratio);
+        sauver_PNG(chemin_destination + "/images/" + nom_image + "_" + 
+                   to_string(i) + ".png", image_tampon);
     }
 
-    if (nb_etapes >= 1) {
-        generer_GIF("out/images/" + nom_fichier + "_", "out/gif/" + nom_fichier, 0, nb_etapes - 1, 15, 0);
-    }
+    generer_GIF(chemin_destination + "/images/" + nom_image + "_",
+                chemin_destination + "/gif/" + nom_image,
+                0, nb_etapes - 1, 15, 0);
 }
 
+void creer_fondu_bruitage(const string& chemin_image, const string& chemin_destination, size_t nb_etapes, double somme_max) {
+    creer_dossiers_sortie(chemin_destination);
 
-// void niveau_gris
-
-
-// void taille_imagr
-
-
-
-// void bruiter_img
-
-
-//
-
-
-void assombrir_image(Image_PNG *image_original, Image_PNG *image_assombri, double ratio) {
-    size_t hauteur = image_original->hauteur;
-    size_t largeur = image_original->largeur;
-
-    for (size_t i = 0; i < hauteur; ++i) {
-        for (size_t j = 0; j < largeur; ++j) {
-            RVB pixel_original = image_original->pixels[i][j];
-
-            RVB pixel_assombri;
-            pixel_assombri.rouge = static_cast<Composante>(pixel_original.rouge * ratio);
-            pixel_assombri.vert = static_cast<Composante>(pixel_original.vert * ratio);
-            pixel_assombri.bleu = static_cast<Composante>(pixel_original.bleu * ratio);
-
-            image_assombri->pixels[i][j] = pixel_assombri;
-        }
-    }
-}
-
-void creer_fondu_noir(const string fichier_path, size_t nb_etapes) {
-    Image_PNG image = charger_PNG(fichier_path);
-    size_t lastSlash = fichier_path.find_last_of("/\\");
-    size_t lastDot = fichier_path.find_last_of(".");
-    string nom_fichier = fichier_path.substr(lastSlash + 1, lastDot - lastSlash - 1 );
-
-    if (!filesystem::exists("out")) {
-        filesystem::create_directory("out");
-    }
-
-    if (!filesystem::exists("out/images")) {
-        filesystem::create_directory("out/images");
-    }
-
-    if (!filesystem::exists("out/gif")) {
-        filesystem::create_directory("out/gif");
-    }
-
-    Image_PNG image_buffer = creer_PNG(image.hauteur, image.largeur);
+    Image_PNG image_source = charger_PNG(chemin_image);
+    string nom_image = extraire_nom_fichier(chemin_image);
+    Image_PNG image_tampon = creer_PNG(image_source.hauteur, image_source.largeur);
     
     for(size_t i = 0; i < nb_etapes; i++) {
-        double ratio = (1.0 / nb_etapes) * i;
-        assombrir_image(&image, &image_buffer, ratio);
-        sauver_PNG("out/images/" + nom_fichier + "_" + to_string(nb_etapes - i - 1) + ".png", image_buffer);
+        double intensite = (somme_max / nb_etapes) * (i + 1);
+        bruiter_image(image_source, image_tampon, intensite);
+        sauver_PNG(chemin_destination + "/images/" + nom_image + "_" + 
+                   to_string(i) + ".png", image_tampon);
     }
 
-    if (nb_etapes >= 1) {
-        generer_GIF("out/images/" + nom_fichier + "_", "out/gif/" + nom_fichier, 0, nb_etapes - 1, 15, 0);
+    generer_GIF(chemin_destination + "/images/" + nom_image + "_",
+                chemin_destination + "/gif/" + nom_image,
+                0, nb_etapes - 1, 15, 0);
+}
+
+void creer_animation_fondu_flou(const string& chemin_image, const string& chemin_destination, size_t nb_etapes, size_t intensite_max) {
+    creer_dossiers_sortie(chemin_destination);
+
+    Image_PNG image_source = charger_PNG(chemin_image);
+    string nom_image = extraire_nom_fichier(chemin_image);
+    Image_PNG image_tampon = creer_PNG(image_source.hauteur, image_source.largeur);
+
+    for(size_t i = 0; i < nb_etapes; i++) {
+        double intensite = (intensite_max / nb_etapes) * (i + 1);
+        flouter_image(image_source, image_tampon, intensite);
+        sauver_PNG(chemin_destination + "/images/" + nom_image + "_" + 
+                   to_string(i) + ".png", image_tampon);
     }
+
+    generer_GIF(chemin_destination + "/images/" + nom_image + "_",
+                chemin_destination + "/gif/" + nom_image,
+                0, nb_etapes - 1, 15, 0);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        throw runtime_error("USAGE: " + string(argv[0]) + " file");
+        throw runtime_error("Usage: " + string(argv[0]) + " fichier_config");
     }
 
-    size_t nb_etapes;
-    cout << "Combien d'étapes pour le fondu ?\n";
-    cin >> nb_etapes;
+    Config config = lire_config(argv[1]);
+    string nom_fonction = config.nom_fonction;
 
-    // creer_fondu_noir(argv[1], nb_etapes);
-    creer_fondu_flou(argv[1], nb_etapes, 50);
-    // creer_fondu_bruitage(argv[1], nb_etapes, 1500);
+    if (nom_fonction == "creer_animation_fondu_noir") {
+        optional<string> chemin_destination = trouver_param_chaine(config, "Schemin_destination");
+        optional<string> chemin_image = trouver_param_chaine(config, "Schemin_image");
+        optional<size_t> nb_etapes = trouver_param_entier(config, "Enb_etapes");
+        creer_animation_fondu_noir(chemin_image.value_or("image.png"), chemin_destination.value_or("destination"), nb_etapes.value_or(5));
+    }
+    // TODO: continue for all functions
+
+    return 0;
 }
