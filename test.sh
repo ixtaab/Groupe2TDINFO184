87ab48e5595d3dec3032802513ce58ff32fbe610
johan0A <arnaudchesse@outlook.com>
2024-12-06 18:56:23 +0100

    added retrecir_image and creer_animation_retrecir

--- test.sh
+++ test.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-echo "[1/7] creating output directory..."
+echo "[1/8] creating output directory..."
 mkdir -p ./test_out
 
-echo "[2/7] clearing output directory..."
+echo "[2/8] clearing output directory..."
 rm -rf ./test_out/*
 
-echo "[3/7] building..."
+echo "[3/8] building..."
 ./build.sh
 
-echo "[4/7] testing fondu_noir configuration..."
-./bin/program ./test_ressources/configs/fondu_noir.txt
+echo "[4/8] testing fondu_noir configuration..."
+time ./bin/program ./test_ressources/configs/fondu_noir.txt
+echo ""
 
-echo "[5/7] testing fondu_noir configuration..."
-./bin/program ./test_ressources/configs/fondu_niveaux_gris.txt
+echo "[5/8] testing fondu_noir configuration..."
+time ./bin/program ./test_ressources/configs/fondu_niveaux_gris.txt
+echo ""
 
-echo "[6/7] testing fondu_bruitage configuration..."
-./bin/program ./test_ressources/configs/fondu_bruitage.txt
+echo "[6/8] testing fondu_bruitage configuration..."
+time ./bin/program ./test_ressources/configs/fondu_bruitage.txt
+echo ""
 
-echo "[7/7] testing fondu_flou configuration..."
-./bin/program ./test_ressources/configs/fondu_flou.txt
+echo "[7/8] testing fondu_flou configuration..."
+time ./bin/program ./test_ressources/configs/fondu_flou.txt
+echo ""
 
-echo "[7/7] testing fondu_flou configuration..."
-./bin/program ./test_ressources/configs/fondu_flou.txt
+echo "[8/8] testing fondu_noir_retrecir configuration..."
+time ./bin/program ./test_ressources/configs/fondu_noir_retrecir.txt
+echo ""
 
 echo "all tests completed"
